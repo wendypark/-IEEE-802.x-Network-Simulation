3e8e54d76fdf9c92cf8a7a3a84d7d64140a8463e
wonhee <wonheeprk@gmail.com>
2017-02-24 00:54:29 -0800

    code cleanup

--- p1.py
+++ p1.py
@@ -1,21 +1,19 @@
 import random
 import math
+#import scipy.stats 
+#import pareto_distribution
 
-
-MAXBUFFER = float('inf')	# max size of buffer
-LENGTH = 0 					# number of packets in queue
+#global variables 
+MAXBUFFER = float('inf')	# max size of buffer for exp 1
+LENGTH = 0 					# number of packets in queue + server
 TIME = 0					# current time
 SERVICE_RATE = 1			# u
-
 PACKETS_DROPPED = 0 		# number of packets dropped
+MEAN_QUEUE_LENGTH = 0		# length of packet * time 
+SERVER_BUSY_TIME = 0		# server occupied
 
-MEAN_QUEUE_LENGTH = 0
-SERVER_BUSY_TIME = 0
-
-IN_BUFFER = 0
-
-
-# SECTION: 3.1
+# SECTION: 3.1 
+# Event Object 
 class Event(object):
 	def __init__(self, event_time=None, event_type=None):
 		self.e_time = event_time
@@ -34,27 +32,25 @@ class Event(object):
 	def eventTime(self):
 		return self.e_time
 
-	# def nextEvent(self):
-
-	# def previousEvent(self):
-
 
 # SECTION: 3.1
+# GEL Object
 class GlobalEventList(object):
 	def __init__(self, event_list=None):
 		self.head = None
 
-	# empty list
 	def insertEvent(self, incoming_event):
+		# empty list
 		if self.head is None:
-			incoming_event.next = self.head # points to nothing
-			self.head = incoming_event		# becomes head
+			incoming_event.next = self.head 	# points to nothing
+			self.head = incoming_event			# becomes head
 
 		# at least one element in there
 		elif self.head.e_time >= incoming_event.e_time:
-			incoming_event.next = self.head # points to previous head
-			self.head = incoming_event		# becomes head
+			incoming_event.next = self.head 	# points to previous head
+			self.head = incoming_event			# becomes head
 
+		#GLE full
 		else:
 			cur_event = self.head
 			while cur_event.next is not None and cur_event.next.e_time < incoming_event.e_time:
@@ -62,23 +58,8 @@ class GlobalEventList(object):
 
 			incoming_event.next = cur_event.next
 			cur_event.next = incoming_event
-
-		# cur = self.head
-		# print "sorted"
-		# while cur.next is not None:
-		# 	print cur.e_time
-		# 	cur = cur.next
-
-		#self.lst.append(incoming_event)
-		# print "UNSORTED"
-		# for i in range(0,len(self.lst)):
-		# 	print self.lst[i].eventTime()
-		# sort after appending new event
-		#self.lst.sort(key=lambda x: x.e_time)
-		# print "SORTED"
-		# for i in range(0,len(self.lst)):
-		# 	print self.lst[i].eventTime()
 	
+	#packet ready for transmission 
 	def removeFirstEvent(self):
 		self.head = self.head.next
 
@@ -88,6 +69,7 @@ class GlobalEventList(object):
 
 
 # SECTION: 3.1
+# Buffer Object
 class Buffer(object):
 	# NOTE: using list as a queue
 	# top of the queue is the last element in list
@@ -101,9 +83,10 @@ class Buffer(object):
 
 	def removePacket(self):
 		if len(self.buff) != 0:
-			self.buff.pop() # remove last element in list or first in queue
+			# remove last element in list or first in queue
+			self.buff.pop() 
 		else:
-			print "buffer is empty"
+			print "Buffer is empty."
 
 	def curBufferSize(self):
 		return len(self.buff)
@@ -116,6 +99,8 @@ class Buffer(object):
 
 
 
+# SECTION: 3.1
+# Packet Object
 class Packet(object):
 	def __init__(self, service_time):
 		self.service_t = service_time
@@ -126,6 +111,7 @@ class Packet(object):
 
 
 # SECTION: 3.3
+# Arrival Event 
 def processArrivalEvent(buff, gel):
 	global TIME 
 	global LENGTH
@@ -133,6 +119,7 @@ def processArrivalEvent(buff, gel):
 	global SERVER_BUSY_TIME
 	global PACKETS_DROPPED
 
+	# time of the "width" of queue
 	time_difference = gel.firstEvent().eventTime() - TIME
 	TIME += time_difference
 
@@ -147,6 +134,7 @@ def processArrivalEvent(buff, gel):
 
 	gel.insertEvent(new_arrival_event)
 
+	#Server is free
 	if LENGTH == 0:
 		serv_time = new_packet.getServiceTime()
 		departure_event_time = TIME + serv_time
@@ -158,14 +146,15 @@ def processArrivalEvent(buff, gel):
 		gel.insertEvent(departure_event)
 
 		LENGTH = 1
-	
+
+	#Server is busy
 	else:
 		# buffer not full
 		if LENGTH - 1 < MAXBUFFER:
 			buff.insertPacket(new_packet)
 			LENGTH = buff.curBufferSize() + 1
 
-		# full buffer
+		# buffer is full
 		else:
 			PACKETS_DROPPED += 1
 
@@ -175,13 +164,15 @@ def processArrivalEvent(buff, gel):
 
 
 # SECTION: 3.4
+# Departure Event 
 def processDepartureEvent(buff, gel):
 	global TIME 
 	global LENGTH
 	global MEAN_QUEUE_LENGTH
 	global SERVER_BUSY_TIME
 
-	time_difference = gel.firstEvent().eventTime() - TIME # time of the "width" of queue
+	# time of the "width" of queue
+	time_difference = gel.firstEvent().eventTime() - TIME 
 	TIME += time_difference
 
 	MEAN_QUEUE_LENGTH += buff.curBufferSize() * time_difference
@@ -190,13 +181,12 @@ def processDepartureEvent(buff, gel):
 
 	LENGTH -= 1
 
+	#system not empty
 	if LENGTH > 0:
 		packet_transmit_time = buff.curPacketServiceTime()
-
 		buff.removePacket()
 
 		departure_event_time = TIME + packet_transmit_time
-		
 		departure_event = Event()
 		departure_event.setEventType(2)
 		departure_event.setEventTime(departure_event_time)
@@ -206,16 +196,26 @@ def processDepartureEvent(buff, gel):
 
 
 # SECTION: 3.6
+# Time intervals in negative exponential distribution 
 def negativeExponenetiallyDistributedTime(rate):
 	u = random.random()
 	return ((-1/rate)*math.log(1-u))
 
-def iterations(exp, MAXBUFFER, ARRIVAL_RATE):
+# SECTION: 3.8 
+# EC: Pareto Distribution 
+def pareto_distribution(rate):
+	return
+
+
+def calculate_stats(exp, MAXBUFFER, ARRIVAL_RATE):
+	
+	# Event type
+	# 1 : arrival
+	# 2: departure 
 	first_arrival_event = 1
 
 	event = Event()
 	gel = GlobalEventList()
-
 	event.setEventType(first_arrival_event)
 	event.setEventTime(TIME + negativeExponenetiallyDistributedTime(ARRIVAL_RATE))
 
@@ -226,67 +226,71 @@ def iterations(exp, MAXBUFFER, ARRIVAL_RATE):
 
 	for i in range(0, 100000):
 		# arrival
-		if gel.firstEvent().curEventType() == 1: # first event
+		if gel.firstEvent().curEventType() == 1: 
 			processArrivalEvent(buff, gel)
 			
 		# departure
 		elif gel.firstEvent().curEventType() == 2:
 			processDepartureEvent(buff, gel)
 
+		# packet transmitted
 		gel.removeFirstEvent()
 
-	print (MEAN_QUEUE_LENGTH)
-	print (TIME)
+	#STATS
 	print ("Buffersize: %s" %str(MAXBUFFER))
 	print ("Experiment %d, Arrival Rate %.2f" % (exp, ARRIVAL_RATE))
 	print ("Mean Queue-Length = %f" % (MEAN_QUEUE_LENGTH/TIME))
 	print ("Server Utilization = %f" % (SERVER_BUSY_TIME/TIME))
 	print ("Number of Packets Dropped = %f \n \n" % PACKETS_DROPPED)
 
+
 # SECTION: 3.1/3.2
 if __name__ == '__main__':
 
-
+	# Experiment 1
 	ARR_RATE = [.1, .25, .4, .55, .65, .8, .9]
-	# .2, .4, .6, .8, .9]
 
 	for x in ARR_RATE:
+
+		#declare global to avoid scoping issues 
 		global ARRIVAL_RATE
 		ARRIVAL_RATE = x
-		iterations(exp=1, MAXBUFFER=float('inf'), ARRIVAL_RATE=x)
 
+		calculate_stats(exp=1, MAXBUFFER=float('inf'), ARRIVAL_RATE=x)
+
+		#reset global vairables 
 		MAXBUFFER = float('inf')	# max size of buffer
-		LENGTH = 0 					# number of packets in queue
+		LENGTH = 0 					# number of packets in queue + server
 		TIME = 0					# current time
 		SERVICE_RATE = 1			# u
-		#ARRIVAL_RATE = 0.25 		# lambda
-
 		PACKETS_DROPPED = 0 		# number of packets dropped
+		MEAN_QUEUE_LENGTH = 0		# length of packet * time 
+		SERVER_BUSY_TIME = 0		# server busy time
 
-		MEAN_QUEUE_LENGTH = 0
-		SERVER_BUSY_TIME = 0
 
+	# Experiment 3
 	ARR_RATE_2 = [.2, .4, .6, .8, .9]
-	#MAXBUFFER = [1, 20, 50]
+
 	for x in ARR_RATE_2:
+
+		#declare global to avoid scoping issues 
 		global ARRIVAL_RATE
 		ARRIVAL_RATE = x
+
 		MAXBUFFER = [1, 20, 50]
 		for y in MAXBUFFER:
-			global MAXBUFFER
-			MAXBUFFER = y 
-			iterations(exp=3, MAXBUFFER=MAXBUFFER, ARRIVAL_RATE=x)
 
-		#MAXBUFFER = float('inf')	# max size of buffer
-			LENGTH = 0 					# number of packets in queue
+			#declare global to avoid scoping issues 
+			global MAXBUFFER
+			calculate_stats(exp=3, MAXBUFFER=y, ARRIVAL_RATE=x)
+
+			#reset global vairables 
+			LENGTH = 0 					# number of packets in queue + server 
 			TIME = 0					# current time
 			SERVICE_RATE = 1			# u
-
-
 			PACKETS_DROPPED = 0 		# number of packets dropped
-
-			MEAN_QUEUE_LENGTH = 0
-			SERVER_BUSY_TIME = 0
+			MEAN_QUEUE_LENGTH = 0		# length of packet * time 
+			SERVER_BUSY_TIME = 0		# server busy time
 
 
 
